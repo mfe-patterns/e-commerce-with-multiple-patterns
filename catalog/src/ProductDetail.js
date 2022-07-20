a87fd2613355e02d3400a62455d6e19dd403a8af
Vishal Sharma <vishal.sharma2@bp.com>
2022-07-20 23:32:29 +0530

    fetching product details from the api

--- catalog/src/ProductDetail.js
+++ catalog/src/ProductDetail.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function ProductDetail() {
+const useStyles = makeStyles((theme) => ({
+  cardMedia: {
+    height: '200px',
+    width: '200px',
+  },
+}));
+
+export default function ProductDetails() {
+  const classes = useStyles();
   const { productId } = useParams();
+  const [product, setProduct] = useState({});
 
+  useEffect(async () => {
+    const product = await fetch(`https://fakestoreapi.com/products/${productId}`).then(res => res.json());
+
+    setProduct(product);
+  }, []);
   return (
     <>
-      <main>
-        In product details {productId}
-      </main>
+      <CardMedia
+        className={classes.cardMedia}
+        image={product.image}
+        title={product.title}
+      />
+      <Typography gutterBottom variant="h5" component="h2">
+        {product.title}
+      </Typography>
+      <Typography>
+        {product.description}
+      </Typography>
     </>
   );
 }
